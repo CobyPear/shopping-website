c9f0fd3bb77cd2731ad96bdb4277a5b0448207ed
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-08 15:44:12 -0600

    added error handling to api and started Redux product reducer

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './config/db.js'
 import logger from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 dotenv.config()
@@ -10,13 +11,17 @@ connectDB()
 
 const PORT = process.env.PORT || 8080
 const app = express()
-
 app.use(logger('dev'))
+
 
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
