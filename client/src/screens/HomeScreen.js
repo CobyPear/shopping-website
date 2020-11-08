97bddc061c59b83ea82c39723d6de1c44da45b46
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-08 16:23:36 -0600

    added loader and message to homescreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
@@ -18,25 +20,25 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             {loading ? (
-                <h2>Loading...</h2>
-            ): error ? (
-                <h3>{error}</h3>
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
             ) : (
-                <Row>
-                {products.map(product => {
-                    return (
-                        <Col
-                            sm={12}
-                            md={6}
-                            lg={4}
-                            xl={3}
-                            key={product._id}>
-                            <Product product={product} />
-                        </Col>
-                    );
-                })}
-            </Row>
-            )}
+                        <Row>
+                            {products.map(product => {
+                                return (
+                                    <Col
+                                        sm={12}
+                                        md={6}
+                                        lg={4}
+                                        xl={3}
+                                        key={product._id}>
+                                        <Product product={product} />
+                                    </Col>
+                                );
+                            })}
+                        </Row>
+                    )}
         </>
     )
 }
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,17 +7,17 @@ import Product from '../models/productModel.js'
 // @desc     Fetch all products
 // @route    GET /api/products
 // @access   Public
-router.get('/', asyncHandler(async (req,res) => {
+router.get('/', asyncHandler(async(req, res) => {
     const products = await Product.find({})
 
     res.json(products)
-    
+
 }))
 
 // @desc     Fetch single product
 // @route    GET /api/products/:id
 // @access   Public
-router.get('/:id', asyncHandler(async (req,res) => {
+router.get('/:id', asyncHandler(async(req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
