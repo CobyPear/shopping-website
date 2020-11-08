1517624741417a1ab0e00e95ee9cf53e80646860
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-08 17:48:18 -0600

    cart actions, reducers, and screen

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -7,7 +7,6 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
-
             const existItem = state.cartItems.find(x => x.product === item.product)
 
             if (existItem) {
