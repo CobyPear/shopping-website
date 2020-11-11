78a761a5709c2f54d562197be1fbb07f968b27de
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-11 10:32:31 -0600

    user routes and jwt

--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 dotenv.config()
-
 connectDB()
 
 const PORT = process.env.PORT || 8080
 const app = express()
 app.use(logger('dev'))
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
@@ -19,9 +20,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
