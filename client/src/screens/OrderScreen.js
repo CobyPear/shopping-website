6f292f41833dcfabcbdb319e66a7a8f00d483c78
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-14 16:36:04 -0600

    orders page and paypal api

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -1,18 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
+import { PayPalButton } from 'react-paypal-button-v2'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getOrderDetails } from '../actions/orderActions'
+import { getOrderDetails, payOrder } from '../actions/orderActions'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
+import axios from 'axios'
 
 const OrderScreen = ({ match }) => {
     const orderId = match.params.id
+
+    const [sdkReady, setSdkReady] = useState(false)
 
     const dispatch = useDispatch()
 
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, loading, error } = orderDetails
+
+    const orderPay = useSelector(state => state.orderPay)
+    const { loading: loadingPay, success: successPay } = orderPay
 
     if (!loading) {
         const addDecimals = num => (Math.round(num * 100) / 100).toFixed(2)
@@ -22,10 +30,34 @@ const OrderScreen = ({ match }) => {
     }
 
     useEffect(() => {
-        if (!order || order._id !== orderId) {
-            dispatch(getOrderDetails(orderId))
+        const addPaypalScript = async () => {
+            const { data: clientId } = await axios.get('/api/config/paypal')
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.async = true
+            script.onload = () => {
+                setSdkReady(true)
+            }
+            document.body.appendChild(script)
         }
-    }, [order, orderId, dispatch])
+
+        if (!order || successPay) {
+            dispatch({ type: ORDER_PAY_RESET })
+            dispatch(getOrderDetails(orderId))
+        } else if (!order.isPaid) {
+            if (!window.paypal) {
+                addPaypalScript()
+            } else {
+                setSdkReady(true)
+            }
+        }
+
+    }, [order, orderId, dispatch, successPay])
+
+    const successPaymentHandler = paymentResult => {
+        dispatch(payOrder(orderId, paymentResult))
+    }
 
     return (loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : <>
         <h1>Order {order._id}</h1>
@@ -48,8 +80,8 @@ const OrderScreen = ({ match }) => {
                     <ListGroup.Item>
                         <h2>Payment Method</h2>
                         <p>
-                        <strong>Method: </strong>
-                        {order.paymentMethod}
+                            <strong>Method: </strong>
+                            {order.paymentMethod}
                         </p>
                         {order.isPaid ? <Message variant='success'>Paid on {order.paidAt}</Message> : <Message variant='danger'>Not paid</Message>}
                     </ListGroup.Item>
@@ -116,6 +148,16 @@ const OrderScreen = ({ match }) => {
                                 <Col>${order.totalPrice}</Col>
                             </Row>
                         </ListGroup.Item>
+                        {!order.isPaid && (
+                            <ListGroup.Item>
+                                {loadingPay && <Loader />}
+                                {!sdkReady ? <Loader /> : (
+                                    <PayPalButton
+                                        amount={order.totalPrice}
+                                        onSuccess={successPaymentHandler} />
+                                )}
+                            </ListGroup.Item>
+                        )}
                     </ListGroup>
                 </Card>
             </Col>
