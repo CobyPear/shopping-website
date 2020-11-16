9ec7c05a512f1212739d6b3ae64f5df4e51dfef9
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-16 14:21:44 -0600

    users list action and reducer

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -26,6 +26,15 @@ const protect = asyncHandler(async(req, res, next) => {
     }
 })
 
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error('Not authorized: not admin')
+    }
+}
 export {
-    protect
+    protect,
+    isAdmin
 }
