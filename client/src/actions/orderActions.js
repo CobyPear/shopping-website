cfa349fd6d893a7347b33346b7eeab166632f2b4
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-11-21 17:32:30 -0600

    admin list orders constants, reducer, and action

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -11,6 +11,9 @@ import {
     ORDER_LIST_USER_REQUEST,
     ORDER_LIST_USER_SUCCESS,
     ORDER_LIST_USER_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_FAIL,
 } from '../constants/orderConstants'
 import axios from 'axios'
 
@@ -134,6 +137,38 @@ export const listUserOrders = () => async (dispatch, getState) => {
         }
 
         const { data } = await axios.get(`/api/orders/myorders`, config)
+
+        dispatch({
+            type: ORDER_LIST_USER_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.message 
+            ? error.response.data.message 
+            : error.message
+        })
+    }
+}
+
+export const listOrders = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: ORDER_LIST_USER_REQUEST
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.get(`/api/orders/`, config)
 
         dispatch({
             type: ORDER_LIST_USER_SUCCESS,
